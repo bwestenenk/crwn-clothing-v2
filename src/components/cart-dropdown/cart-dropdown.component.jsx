d50553d9ddb7c37d2413e27b78f3410c51929454
bwestenenk <bwestenenk@gmail.com>
2022-07-14 13:26:28 -0400

    agregamos cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,19 @@
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 import "./cart-dropdown.styles.scss";
 import Button from "../button/button.component";
-
+import CartItem from "../cart-item/cart-item.component";
 
 const CartDropdown = () => {
+
+    const {cartItems} = useContext(CartContext);
+
     return (
         <div className="cart-dropdown-container">
             <div className="cart-items">
-                <Button>Out</Button>
+                {cartItems.map(item => <CartItem key={item.id} cartItem={item}/>)}
             </div>
+            <Button>Out</Button>
         </div>
     )
 }
